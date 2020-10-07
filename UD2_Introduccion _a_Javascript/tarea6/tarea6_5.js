eea661dd7b17b792f256ab5fa898cc39a26ef314
romitus <romitus>
2020-10-07 19:49:55 +0200

    Changes of js archives

---
+++ UD2_Introduccion _a_Javascript/tarea6/tarea6_5.js
@@ -0,0 +1,28 @@
+function Multiply(nota){
+    num=Number(nota);
+    if(num<2 || num>10){
+        console.log("Ese número es incorrecto");
+        
+    }else{
+    console.log("Multiplicar");
+        nota = parseInt(nota);
+    for (let index = 0; index < 11; index++) {
+        console.log(nota+" * "+index+" = "+nota*index);
+        
+    }
+    console.log("Suma");
+    var i=0;
+    while (i<11) {
+        console.log((Number(nota)+Number(i)));
+        i++;
+    }
+    console.log("Resta");
+    var j=0;
+    do {
+        console.log((Number(nota)-Number(j)));
+        j++;
+    } while (j<11);
+    }
+
+    
+}
