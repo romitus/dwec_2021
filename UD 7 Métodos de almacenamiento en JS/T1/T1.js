a75708fe2fab098e5b26cd62751261e45f0ea66c
romitus <alum.cmunozi@iesalixar.org>
2021-02-11 01:30:58 +0100

    Exercise Storage

--- UD 7 Métodos de almacenamiento en JS/T1/T1.js
+++ UD 7 Métodos de almacenamiento en JS/T1/T1.js
@@ -1,42 +1,83 @@
 window.addEventListener('load', iniciar, false);
 
 function iniciar() {
-document.getElementById('save1').addEventListener('click', saveStorage());
-document.getElementById('delete1').addEventListener('click', deleteStorage());
-document.getElementById('refresh1').addEventListener('click', refreshStorage());
-document.getElementById('check1').addEventListener('click', checkStorage());
+document.getElementById('save1').addEventListener('click', saveStorage1);
+document.getElementById('delete1').addEventListener('click', deleteStorage1);
+document.getElementById('refresh1').addEventListener('click', refreshStorage1);
+document.getElementById('check1').addEventListener('click', checkStorage1);
 }
 
-function saveStorage() {
+function saveStorage1() {
     let chooseUser = document.getElementById("choose1").value;
     if (chooseUser == 0) {
-        localStorage();
-    }else{
-        sessionStorage();
+        localStorage1();
+    }else if (chooseUser == 1){
+        sessionStorage1();
     }
 }
 
-function deleteStorage() {
+function deleteStorage1() {
+    let keyInput = document.getElementById("key1").value;
+    localStorage.removeItem(keyInput);
+    sessionStorage.removeItem(keyInput);
+    refreshStorage1();
+    console.log("Almacenamiento de clave " + keyInput + "Borrado");
+}
+
+function refreshStorage1() {
+  document.getElementById("area1").innerHTML = allStorageLocal1();
+  document.getElementById("area2").innerHTML = allStorageSession1();
+  console.log("Almacenamiento Actualizado ");
+}
+
+
+function allStorageLocal1() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]) );
+    }
+
+    return values;
+}
+
+function allStorageSession1() {
+
+    var values = [],
+        keys = Object.keys(sessionStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push( sessionStorage.getItem(keys[i]) );
+    }
+
+    return values;
+}
+
+function checkStorage1() {
+    if (typeof(Storage) !== "undefined") {
+        document.getElementById("msg").innerHTML ="Storage soportado en este navegador";
+        document.getElementById("msg").style = "color:green;";
+    } else {
+        document.getElementById("msg").innerHTML ="Storage no soportado en este navegador";
+        document.getElementById("msg").style = "color:red;";
+    }
     
 }
 
-function refreshStorage() {
-    
-}
-
-function checkStorage() {
-    
-}
-
-function localStorage() {
+function localStorage1() {
     let keyInput = document.getElementById("key1").value;
     let valueInput = document.getElementById("value1").value;
-    //localStorage.setItem(keyInput, valueInput);
-    localStorage.setItem("mytime", Date.now());
+    localStorage.setItem(keyInput, valueInput);
+    console.log("Almacenado en Local");
 }
 
-function sessionStorage() {
-    let keyInput = document.getElementById("key1");
-    let valueInput = document.getElementById("value1");
-    sessionStorage.setItem(keyInput,keyInput);
+function sessionStorage1() {
+    let keyInput = document.getElementById("key1").value;
+    let valueInput = document.getElementById("value1").value;
+    sessionStorage.setItem(keyInput,valueInput);
+    console.log("Almacenado en Session");
 }
